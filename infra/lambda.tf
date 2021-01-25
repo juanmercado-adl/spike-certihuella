85f1e5aea8ac40296f7a6ec2c5e2a9bba7816a8f
juliomartinez5 <julio.martinez@avaldigitallabs.com>
2021-01-25 09:23:32 -0500

    :building_construction: se implementa la infra terraform y pruebas de lambda con dynamo

--- infra/lambda.tf
+++ infra/lambda.tf
@@ -17,5 +17,5 @@ resource "aws_lambda_permission" "java_lambda_function" {
   function_name = "${aws_lambda_function.java_lambda_function.function_name}"
   principal     = "apigateway.amazonaws.com"
   # The /*/#* portion grants access from any method on any resource
-  # within the API Gateway "REST API".
+  # within the API Gateway "-REST API".
   //source_arn = "${aws_api_gateway_deployment.java_lambda_deploy.execution_arn}/*/*"
